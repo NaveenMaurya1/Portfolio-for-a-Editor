e864b741694d4883f944a91add81eed42c121a60
Naveen_M <naveenmauryagng@gmail.com>
2026-09-20 17:41:07 +0530

    changing gif in about page

--- app/about/page.js
+++ app/about/page.js
@@ -123,7 +123,7 @@ export default function About() {
             <BlurFade>
 
               <video
-                src="/about/idea.mp4"
+                src="/about/rain.mp4"
                 className="w-full h-96 object-cover"
                 autoPlay
                 muted
