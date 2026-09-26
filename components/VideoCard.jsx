65f242f435a31ee981b235f97314e3c56fc724d6
Naveen_M <naveenmauryagng@gmail.com>
2026-09-26 17:57:30 +0530

    adding favicon.ico

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -18,6 +18,7 @@ export default function VideoCard({ video }) {
                     src={video.thumbnail}
                     alt={video.title}
                     fill
+                    sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
                     className="object-cover transition-transform duration-700 group-hover:scale-105"
                 />
 
